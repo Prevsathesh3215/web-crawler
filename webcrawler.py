f61f8e73379bb03b2b64fd3c0fc73a3eb210bea1
Sathesh Previn <153922083+Prevsathesh3215@users.noreply.github.com>
2026-03-05 14:57:03 +0800

    Enhance web crawler with scoring and embedding
    Refactor web crawler to improve link scoring and embedding features. Adjust depth control and number of links processed.

--- webcrawler.py
+++ webcrawler.py
@@ -11,13 +11,21 @@ import networkx as nx
 import json
 from pyvis.network import Network
 from IPython.display import IFrame
+import unicodedata
+import heapq
+from sentence_transformers import SentenceTransformer
+import numpy as np
+
 
 
 #BOOKS TO SCRAPE
 # url = "https://books.toscrape.com/"
 
 #WIKIPEDIA: EARTH
-url = "https://en.wikipedia.org/wiki/Earth"
+# url = "https://en.wikipedia.org/wiki/Earth"
+
+#WIKIPEDIA: JESUS
+url  = "https://en.wikipedia.org/wiki/Jesus"
 
 #ATHEX GROUP STOCKS
 # url = "https://www.athexgroup.gr/en"
@@ -26,7 +34,12 @@ domain = urlparse(url).netloc
 
 article_path = "//article"
 article_link = "h3/a/@href"
-all_links_xpath = "//table[contains(@class,'infobox')]//a[starts-with(@href,'/wiki/') and not(contains(@href,':'))]/@href"
+# all_links_xpath = "//table[contains(@class,'infobox')]//a[starts-with(@href,'/wiki/') and not(contains(@href,':'))]/@href"
+all_links_xpath = """
+//table[contains(@class,'infobox')]//a[starts-with(@href,'/wiki/') and not(contains(@href,':'))]
+|
+//*[@id='mw-content-text']//div[contains(@class,'mw-parser-output')]/p//a[starts-with(@href,'/wiki/') and not(contains(@href,':'))]
+"""
 all_summaries = "//p"
 headers = {
     "User-Agent": "MyKnowledgeGraphBot/1.0 (prevsathesh3215@gmail.com)"
@@ -63,34 +76,46 @@ class KnowledgeGraph:
     def __init__(self):
         self.graph = nx.DiGraph()
 
+
+    @staticmethod
+    def normalize_node(text):
+      text = text.replace("_", " ")
+      text = text.lower()
+      text = unicodedata.normalize("NFKC", text)
+      text = re.sub(r"\s+", " ", text)
+      return text.strip()
+
     def build_graph(self, pages):
         for page in pages:
-            title = page["title"]
+            title = self.normalize_node(page["title"])
 
             self.graph.add_node(title)
 
             for link in page["links"]:
-                target = link.split("/wiki/")[-1]
+                target = self.normalize_node(link.split("/wiki/")[-1])
 
                 self.graph.add_edge(title, target)
 
 
+
+
+
+
 #CRAWLER CLASSES
 class Frontier:
-  def __init__(self):
-    self.queue = deque()
-    self.visited = set()
+    def __init__(self):
+        self.queue = []
+        self.visited = set()
 
-  def add_url(self, url):
-    if url not in self.visited:
-      self.queue.append(url)
-      self.visited.add(url)
+    def add_url(self, url, score=0):
+        if url not in self.visited:
+            heapq.heappush(self.queue, (-score, url))
+            self.visited.add(url)
 
-  def get_next(self):
-    if self.queue:
-      return self.queue.popleft()
-    else:
-      return None
+    def get_next(self):
+        if self.queue:
+            return heapq.heappop(self.queue)[1]
+        return None
 
 
 
@@ -198,7 +223,7 @@ class Page:
             "title": self.title,
             "url": self.url,
             "summary": self.summary,
-            "links": self.child_links[:20]
+            "links": self.child_links[:10] # CHANGE NUMBER OF LINK EDGES HERE
         })
 
         # print(each_page_data)
@@ -223,6 +248,38 @@ class CrawlerEngine:
     self.extractor = LinkExtractor()
     self.sanitizer = LinkSanitizer(domain)
 
+    self.cluster_centroid = None
+    self.cluster_vectors = []   
+
+    self.embedder = SentenceTransformer("all-MiniLM-L6-v2")
+    self.embedding_cache = {}
+
+  
+  @staticmethod
+  def simple_link_score(url, summary=""):
+      name = url.split("/wiki/")[-1]
+
+      score = len(name)
+
+      # Bonus if summary contains link name
+      if name.lower() in summary.lower():
+          score += 50
+
+      return score
+
+  def embed(self, text):
+
+      if text in self.embedding_cache:
+          return self.embedding_cache[text]
+
+      if not text:
+          vector = np.zeros(384)
+      else:
+          vector = self.embedder.encode(text)
+
+      self.embedding_cache[text] = vector
+      return vector
+
 
   def store_data(self):
     with open("pages.json", "w") as f:
@@ -230,25 +287,34 @@ class CrawlerEngine:
 
   def start_crawl_bfs(self):
 
-    depth = 0
+      while self.frontier.queue and len(self.each_page_data) < self.depth_control:
 
-    while self.frontier.queue and depth < self.depth_control:
+          current_url = self.frontier.get_next()
 
-        current_url = self.frontier.get_next()
+          if not current_url:
+              continue
 
-        print("\nAccessing site:", current_url)
+          print("\nAccessing site:", current_url)
 
-        page = Page(current_url, self.client, self.extractor, self.sanitizer)
-        page.crawl(self.each_page_data)
+          page = Page(current_url, self.client, self.extractor, self.sanitizer)
+          page.crawl(self.each_page_data)
 
-        for link in page.child_links:
-            self.frontier.add_url(link)
+          vector = self.embed(page.summary)
+          self.cluster_vectors.append(vector)
 
-        depth += 1
-        time.sleep(7)
+          if len(self.cluster_vectors) > 0:
+              self.cluster_centroid = np.mean(
+                  self.cluster_vectors,
+                  axis=0
+              )
 
-    print("\nFINAL DATA:")
-    print(self.each_page_data)
+
+          for link in page.child_links:
+            score = self.simple_link_score(link, page.summary)             
+            self.frontier.add_url(link, score)
+
+          time.sleep(2)
+      print(self.each_page_data)
 
 
 
@@ -276,13 +342,14 @@ class CrawlerEngine:
       for site in self.frontier.visited:
         print(site)
 
-      time.sleep(7)
+      time.sleep(3)
 
 
 
 
 if __name__ == "__main__":
-  depth_control = 50
+  depth_control = 80
+
   crawler = CrawlerEngine(depth_control, url, all_links_xpath)
   crawler.start_crawl_bfs()
   crawler.store_data()
